5030f99cfe5bfeafdc29b3957ab321705a56f5f4
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-09-05 17:43:23 +0200

    Add swiper slider. Add section 8.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+const App = {
+  init() {
+    App.youtube();
+  },
+
+  youtube: function () {
+    console.log( 'Cargando youtube' );
+    const mySwiper = new Swiper( '.section-7 .swiper-container', {
+      // Optional parameters
+      pagination: '.swiper-pagination',
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      paginationClickable: true,
+      spaceBetween: 30,
+      slidesPerView: 2,
+      loop: true,
+    } );
+  },
+};
+
+App.init();
